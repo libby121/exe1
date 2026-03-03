cc7146925afd4d7f15a43ccaf1d17af47a28788c
Libby Ovadia <libbyl6564@gmail.com>
2026-03-03 20:59:16 +0200

    add NoResourceFoundException to advice

--- src/main/java/com/example/exe1/customer/controller/errors/RestExceptionHandler.java
+++ src/main/java/com/example/exe1/customer/controller/errors/RestExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @RestControllerAdvice
 public class RestExceptionHandler {
@@ -13,6 +14,12 @@ public class RestExceptionHandler {
     public ResponseEntity<ErrorResponse> handleBadJson(HttpMessageNotReadableException ex) {
         return ResponseEntity.badRequest()
                 .body(new ErrorResponse(HttpStatus.BAD_REQUEST, "Invalid JSON / invalid request body"));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleBadJson(NoResourceFoundException ex) {
+        return ResponseEntity.badRequest()
+                .body(new ErrorResponse(HttpStatus.NOT_FOUND, "Invalid path"));
     }
 
 
